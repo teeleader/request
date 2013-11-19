8eabd44aa897569929fbc9e1fb7cd930acc63657
Shaun Berryman <shaun@shaunberryman.com>
2013-11-19 12:22:08 -0800

    minor patch to get digest working
    server qop is auth, auth-init and that doesn’t work with a .NET web
    server apparently. QOP must be ‘auth’ only!

--- request.js
+++ request.js
@@ -716,16 +716,17 @@ Request.prototype.onResponse = function (response) {
 
         var ha1 = md5(self._user + ':' + challenge.realm + ':' + self._pass)
         var ha2 = md5(self.method + ':' + self.uri.path)
-        var digestResponse = md5(ha1 + ':' + challenge.nonce + ':1::auth:' + ha2)
+        var cnonce = uuid().replace(/-/g, '')
+        var digestResponse = md5(ha1 + ':' + challenge.nonce + ':1:' + cnonce + ':auth:' + ha2)
         var authValues = {
           username: self._user,
           realm: challenge.realm,
           nonce: challenge.nonce,
           uri: self.uri.path,
-          qop: challenge.qop,
+          qop: "auth", //challenge.qop,
           response: digestResponse,
           nc: 1,
-          cnonce: ''
+          cnonce: cnonce
         }
 
         authHeader = []
